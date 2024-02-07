6e0b6216664a984fc789aafeb60505fac0e38a6d
samosato <68155940+samuelgecik@users.noreply.github.com>
2024-02-07 16:17:22 +0100

    Refactor Downloader class to support multiple downloads and add output folder parameter

--- src/downloading.py
+++ src/downloading.py
@@ -2,14 +2,23 @@ import aiohttp
 import asyncio
 from tqdm import tqdm
 
+TIMEOUT = aiohttp.ClientTimeout(total=60*60)
+
 class Downloader():
-    def __init__(self, download_links):
+    def __init__(self, download_links, output_folder):
         self.download_links = download_links
+        self.output_folder = output_folder
+        self.session = aiohttp.ClientSession(timeout=TIMEOUT)
+        self.sizes = self.get_sizes(self.session)
         self.total_size = 0
 
-    async def get_size(self, session, url):
-        async with session.get(url) as response:
-            return int(response.headers['Content-Length'])
+    async def get_sizes(self):
+        sizes = []
+        for url in self.download_links:
+            async with self.session.get(url) as response:
+                sizes.append(int(response.headers['Content-Length']))
+        return sizes
+
 
     async def download_file(self, session, url, filename):
         async with session.get(url) as response:
@@ -20,20 +29,11 @@ class Downloader():
                     total_downloaded += len(chunk)
             return total_downloaded
 
-    async def run(self):
-        timeout = aiohttp.ClientTimeout(total=60*60)
-        async with aiohttp.ClientSession(timeout=timeout) as session:
+    async def download(self, indicies: list):
+        async with self.session as session:
             tasks = []
-            # Get sizes of all files
-            sizes = await asyncio.gather(*(self.get_size(session, link) for link in self.download_links[:2]))
-            total_size = sum(sizes)
-            for link in self.download_links[:2]:
+            for link in [self.download_links[x] for x in indicies]:
                 filename = link[link.rindex('/') + 1:]
                 tasks.append(self.download_file(session, link, filename))
             await asyncio.gather(*tasks)
-            print(f'Total downloaded size: {total_size} bytes')
-
-    async def run(self):
-        await self.run()
-
  

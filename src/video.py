d89a4b74d0380bb84d8a2dea5214738606e1844e
samosato <68155940+samuelgecik@users.noreply.github.com>
2024-02-19 00:57:07 +0100

    Add session parameter to Video class constructor

--- src/video.py
+++ src/video.py
@@ -5,8 +5,9 @@ TIMEOUT = aiohttp.ClientTimeout(total=60*60)
 
 
 class Video:
-    def __init__(self, url: str):
+    def __init__(self, url: str, session: aiohttp.ClientSession):
         self.url = url
+        self.session = session
         self.name = self.get_name()
         self.size = self.get_size()
         self.download = True

a421ca12341519092002367a75b84cfaad566dd0
samosato <68155940+samuelgecik@users.noreply.github.com>
2024-02-19 00:48:05 +0100

    Add Video class with info about video

---
+++ src/video.py
@@ -0,0 +1,19 @@
+import aiohttp
+import asyncio
+
+TIMEOUT = aiohttp.ClientTimeout(total=60*60)
+
+
+class Video:
+    def __init__(self, url: str):
+        self.url = url
+        self.name = self.get_name()
+        self.size = self.get_size()
+        self.download = True
+
+        def get_name(self):
+            return self.url[self.url.rfind("/") + 1 :]
+        
+        async def get_size(self):
+            async with self.session.get(url) as response:
+                return int(response.headers['Content-Length'])

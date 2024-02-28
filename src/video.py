085b73a1b4fb30e455368ae52607ff2051bf47f2
samosato <68155940+samuelgecik@users.noreply.github.com>
2024-02-28 15:28:01 +0100

    Refactor Video class constructor to accept a name parameter

--- src/video.py
+++ src/video.py
@@ -4,15 +4,15 @@ TIMEOUT = aiohttp.ClientTimeout(total=60*60)
 
 
 class Video:
-    def __init__(self, url: str, session: aiohttp.ClientSession):
+    def __init__(self, url: str, name: str, session: aiohttp.ClientSession):
         self.url = url
         self.session = session
-        self.name = self.get_name()
+        self.name = name
         self.size = None
         self.download = True
 
-    def get_name(self):
-        return self.url[self.url.rfind("/") + 1 :]
+    # def get_name(self):
+    #     return self.url[self.url.rfind("/") + 1 :]
     
     async def get_size(self):
         if self.size is None:  # Only make the request if size hasn't been retrieved yet
